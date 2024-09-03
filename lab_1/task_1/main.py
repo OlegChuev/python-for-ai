9d2eb8db0db37bff59292a8a9903c8cc980b0c56
OlegChuev <o.chuev@mobidev.biz>
2024-09-03 21:38:38 +0300

    Added lab_1 and base dockerfile + docker-compose

---
+++ lab_1/task_1/main.py
@@ -0,0 +1,30 @@
+import math
+
+def calculate_area_of_triangle(side1, side2, side3):
+    # Calculate the semi-perimeter
+    s = (side1 + side2 + side3) / 2
+
+    # Calculate the area using Heron's formula
+    area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+
+    return area
+
+def main():
+    try:
+        # Get user input for the sides of the triangle
+        side1 = float(input("Enter the length of the first side: "))
+        side2 = float(input("Enter the length of the second side: "))
+        side3 = float(input("Enter the length of the third side: "))
+
+        # Check if the sides form a valid triangle
+        if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
+            area = calculate_area_of_triangle(side1, side2, side3)
+            print(f"The area of the triangle is: {area:.2f}")
+        else:
+            print("The lengths provided do not form a valid triangle.")
+
+    except ValueError:
+        print("Invalid input. Please enter numerical values for the sides.")
+
+if __name__ == "__main__":
+    main()
---
+++ lab_1/task_2/main.py
@@ -0,0 +1,24 @@
+import math
+
+def calculate_Y(x, a):
+    numerator = math.exp(-x) + math.log(abs(math.acos(x))) + math.atan(x) - (1.2 * math.cos(x)**2)
+    denominator = (math.sqrt(a*x) * (a * math.exp(a*x) + 3.5 * 10**-5)) - (x + a**2 - 0.3)**(1/3)
+
+    if denominator == 0:
+        raise ValueError("Denominator evaluates to zero, leading to division by zero.")
+
+    Y = numerator / denominator
+    return Y
+
+def main():
+    a = 0.5
+    print(f"A: {a:.2f}")
+
+    x = 3.4 * 10**-4
+    print(f"X: {x:.10f}")
+
+    y = calculate_Y(a, x)
+    print(f"Y: {y:.10f}")
+
+if __name__ == "__main__":
+    main()
